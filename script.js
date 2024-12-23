373932f7db8575e0787fbb8662b2caf0838bedf2
LAL CHAND <159679871+LS0958@users.noreply.github.com>
2024-12-23 16:17:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-ECHO is on.
+const pages = document.querySelectorAll('.page');
+const max = document.body.scrollHeight - window.innerHeight;
+let scrollp = 0;
+let arr = [0,max / 4, max / 3, max / 2, max];
+let index = 0;
+
+const setTranform = () => {
+    pages.forEach((e,i)=>{
+        // e.setAttribute('data-n',arr[index]);
+        e.style.transform = `translateZ(${scrollp - arr[i]}px)`;
+    })
+}
+
+setTranform();
+
+window.addEventListener('scroll', e => {
+    scrollp = document.body.scrollTop || document.documentElement.scrollTop;
+    setTranform();
+    if(scrollp>arr[index])index++;
+    if(scrollp<arr[index])index--;
+    console.log(scrollp, arr[index]);
+})
