b90566d6044b6b7f9849c80da778c067a61f03e8
LAL CHAND <159679871+LS0958@users.noreply.github.com>
2024-12-23 19:25:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const pages = document.querySelectorAll('.page');
 const max = document.body.scrollHeight - window.innerHeight;
 let scrollp = 0;
+const totalpages = 5; //except home page
 // let index = 0;
 
 const setTranform = () => {
-    pages.forEach((e,i)=>{
+    pages.forEach((e, i) => {
         setTimeout(() => {
-            e.style.transform = `translateZ(${scrollp - max/4*i}px)`;
+            e.style.transform = `translateZ(${scrollp - max / totalpages * i}px)`;
         }, 100);
     })
 }
@@ -15,18 +16,14 @@ setTranform();
 
 window.addEventListener('scroll', e => {
     scrollp = document.body.scrollTop || document.documentElement.scrollTop;
-    if(scrollp<max){
-        setTranform();
-    }
+    if (scrollp < max) setTranform();
     // if(scrollp>arr[index])index++;
     // if(scrollp<arr[index])index--;
 })
 
 
-function target(n){
-    console.log(n,max/4*(n-1));
-    const e = document.getElementById('page'+n)
+function target(n) {
     scrollp = document.body.scrollTop || document.documentElement.scrollTop;
-    window.scrollTo(0,max/4*(n-1))
+    window.scrollTo(0, max / totalpages * (n - 1))
     setTranform();
 }
