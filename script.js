ce6f4f9f48882ba710360a7f9b18b3f0f1df5da2
Sammy414 <75951934+Sammy414@users.noreply.github.com>
2024-12-24 20:53:25 +0530

    merge

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-ECHO is on.
+let page = (1600-window.innerHeight)/4;
+window.addEventListener('scroll', function() {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    console.log(scrollTop,page);  
+    if(scrollTop>=page){
+        page+=page
+    }
+});
